abbf8f6ccc79d54af07c1c0cb198f02792600f9d
nuri-rama <mahasiswapinuriramawati245@gmail.com>
2025-07-05 10:17:57 +0700

    add login and register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', [HomeController::class, 'welcome']);
+Route::get('/service', [HomeController::class, 'service']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/login', [HomeController::class, 'login']);
+Route::get('/register', [HomeController::class, 'register']);
+
+Route::get('/test', [TestingController::class, 'test']);
+Route::get('test/{id}', [TestingController::class, 'testById']);
+Route::get('/test/{id}/edit', [TestingController::class, 'testByIdEdit']);
